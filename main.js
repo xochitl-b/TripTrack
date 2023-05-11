89b8b9239008ccab71681bf0ad186035de79ece1
xochitl-b <xochitl.burtnik@gmail.com>
2023-05-11 15:20:09 +0100

    fixed my node issue on main. Window is opening now

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 const electron = require('electron'); //calling to our electron 'object'
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow; //working with browser window option
-const notifier = require('node-notifier');
 
 let mainWindow = null; //creating variable
 
@@ -13,12 +12,12 @@ app.on('window-all-closed', () => { //we grab app object and create a function
 
 app.on('ready', () => { //another function for when the app is launched
     mainWindow = new BrowserWindow({
-  /* We can put the specifics about the window here. for now I'll comment it out.
-        width: , 
-        height: ,
-*/
+  
+        width: 600, 
+        height: 400
+
     });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-    mainWindow.on('closed', () => { mainWindow = null; }); // close the window when closed. lol
+    mainWindow.on('closed', () => { mainWindow = null; });
 });
+
