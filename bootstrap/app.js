65620311d38fff7f32f9c2e01e440aadbe52651a
xochitl-b <xochitl.burtnik@gmail.com>
2023-05-17 19:18:24 +0100

    Text is saving locally. Button works.

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -1,22 +1,15 @@
 'use strict';
+//the script was not working unless it was on the same path as the index
 
-//save the info on the notes (on local storage)
-
-//save
-//function saveNotes () {
-//    let notes = window.document.querySelector('textarea'). value; // these notes that we created
-//    window.localStorage.setItem('notes', notes); // saved in local storage saved as notes.
-//}
-
-function saveNotes() {
+function addEntry() {
     var entryText = document.getElementById('entry').value;
     if (entryText.trim() !== '') {
       var entryElement = document.createElement('p');
       entryElement.textContent = entryText;
-
+  
       var entriesDiv = document.getElementById('entries');
       entriesDiv.appendChild(entryElement);
-
+  
       document.getElementById('entry').value = '';
     }
   }
