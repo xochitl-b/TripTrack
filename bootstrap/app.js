30297943af29ea956c9f5bc37f3a8b6bcde6a912
xochitl-b <xochitl.burtnik@gmail.com>
2023-05-21 11:16:10 +0100

    Blog post can upload and be deleted individually

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -1,119 +1,119 @@
 'use strict';
 // Keep this file here, the script was not working unless it was on the same path as the html files
 
-// load past content stored locally upon start
-window.addEventListener('DOMContentLoaded', function () {
-  loadEntriesFromLocalStorage();
+        // Load existing blog posts from local storage
+        window.addEventListener('load', () => {
+          const savedBlogs = JSON.parse(localStorage.getItem('blogs')) || [];
+          savedBlogs.forEach(blog => {
+              addBlogToList(blog);
+          });
+      });
 
-  // Event listener to the delete button
-  var deleteButton = document.getElementById('deleteButton');
-  deleteButton.addEventListener('click', deleteAllEntries);
-});
+      // Save a blog post to local storage
+      function saveBlogToLocalStorage(blog) {
+          const savedBlogs = JSON.parse(localStorage.getItem('blogs')) || [];
+          savedBlogs.push(blog);
+          localStorage.setItem('blogs', JSON.stringify(savedBlogs));
+      }
 
+      // Remove a blog post from local storage
+      function removeBlogFromLocalStorage(blog) {
+          const savedBlogs = JSON.parse(localStorage.getItem('blogs')) || [];
+          const updatedBlogs = savedBlogs.filter(savedBlog => savedBlog.title !== blog.title || savedBlog.text !== blog.text || savedBlog.image !== blog.image);
+          localStorage.setItem('blogs', JSON.stringify(updatedBlogs));
+      }
 
-//------ Drag and drop ------
-//When file dropped to drop area "images". Handle drag leave function removes hover effect
-function handleDrop(event) {
-  event.preventDefault();
-  var files = event.dataTransfer.files;
-  document.getElementById('image').files = files;
-  handleDragLeave(event);
-}
-//stop default behaviour to allow dropping files onto the area
-function handleDragOver(event) {
-  event.preventDefault();
-}
-//when file is dragged on drop area indicates hover effect
-function handleDragEnter(event) {
-  event.preventDefault();
-  var dropArea = event.target;
-  dropArea.classList.add('hover');
-}
-//when a file is dragged out of the drop-area. 
-//Stops the default behavior and removes the hover class from the drop-area to remove the hover effect
-function handleDragLeave(event) {
-  event.preventDefault();
-  var dropArea = event.target;
-  dropArea.classList.remove('hover');
-}
-//if using the "select files"
-function handleFileSelect(event) {
-  var files = event.target.files;
-  // You can access the selected files here and perform any necessary processing
-}
+      // Add a blog post to the list
+      function addBlogToList(blog) {
+          const entries = document.getElementById('entries');
 
-    //function to save entry to local storage
-    function saveEntryToLocalStorage(entryElement) {
-      // Get existing entries from local storage
-      var entries = JSON.parse(localStorage.getItem('entries')) || [];
-      // Add the new entry to the entries array
-      entries.push(entryElement.innerHTML);
-      // Save the updated entries array back to local storage
-      localStorage.setItem('entries', JSON.stringify(entries));
-    }
+          const blogEntry = document.createElement('div');
+          blogEntry.classList.add('blog-entry');
 
-function addEntry() {
-  var entryText = document.getElementById('entry').value; //get the "text" part of the entry
-  var titleText = document.getElementById('title').value; //get the title
-  var imageFiles = document.getElementById('image').files; //get whatever image is uploaded
+          const titleElement = document.createElement('h1');
+          titleElement.textContent = blog.title;
+          blogEntry.appendChild(titleElement);
 
-  if (entryText.trim() !== '') {
-    var entryElement = document.createElement('div');
-    entryElement.classList.add('entry');
+          const textElement = document.createElement('p');
+          textElement.textContent = blog.text;
+          blogEntry.appendChild(textElement);
 
-    //if title is there, the function creates a h2 element and appended to the entry div
-    if (titleText.trim() !== '') {
-      var titleElement = document.createElement('h2');
-      titleElement.textContent = titleText;
-      entryElement.appendChild(titleElement);
-    }
-    //text is put in a p element and appended to entry div
-    var entryTextElement = document.createElement('p');
-    entryTextElement.textContent = entryText;
-    entryElement.appendChild(entryTextElement);
+          const imageElement = document.createElement('img');
+          imageElement.src = blog.image;
+          blogEntry.appendChild(imageElement);
 
-    //create a div "imagesWrapper" with css style to display as 3 by 3
-    var imagesWrapper = document.createElement('div');
-    imagesWrapper.classList.add('images-wrapper');
+          const deleteButton = document.createElement('button');
+          deleteButton.textContent = 'Delete';
+          deleteButton.addEventListener('click', () => {
+              removeBlogFromLocalStorage(blog);
+              entries.removeChild(blogEntry);
+          });
+          blogEntry.appendChild(deleteButton);
 
-    //if image is uploaded, using FileReader API, the function creates an img
-    //loop to go over the images if more than on is uploaded
-    for (var i = 0; i < imageFiles.length; i++) {
-      var imageElement = document.createElement('img');
-      var reader = new FileReader();
-      reader.onload = (function (img) {
-        return function (e) {
-          img.src = e.target.result;
-        };
-      })(imageElement);
-      //image source is set as image URL and appended to entry images wrapper div
-      reader.readAsDataURL(imageFiles[i]);
-      imagesWrapper.appendChild(imageElement);
-    }
-    //append imageswrapper div to entry elemnt
-    entryElement.appendChild(imagesWrapper);
+          entries.appendChild(blogEntry);
+      }
 
-    //Entry (title+text+image) is appended to the entries div
-    var entriesDiv = document.getElementById('entries');
-    entriesDiv.appendChild(entryElement);
+      // Handle form submission (post submission)
+      const blogForm = document.getElementById('blogForm');
+      blogForm.addEventListener('submit', (e) => {
+          e.preventDefault();
 
-    document.getElementById('entry').value = '';
-    document.getElementById('title').value = '';
-    document.getElementById('image').value = '';
+          const titleInput = document.getElementById('titleInput');
+          const textInput = document.getElementById('textInput');
 
+          // Retrieve the dropped image (if any)
+          const droppedImage = document.getElementById('imageInput').files[0];
+          //if image then add to blog
+          if (droppedImage) {
+              const reader = new FileReader();
+              reader.onload = () => {
+                  const blog = {
+                      title: titleInput.value,
+                      text: textInput.value,
+                      image: reader.result
+                  };
 
-    saveEntryToLocalStorage(entryElement);
+                  addBlogToList(blog);
+                  saveBlogToLocalStorage(blog);
 
-  }
-}
+                  titleInput.value = '';
+                  textInput.value = '';
+              };
+              reader.readAsDataURL(droppedImage);
+          } else {
+              alert('Please upload an image.');
+          }
+      });
 
-function deleteAllEntries() {
-  var entriesDiv = document.getElementById('entries');
-  entriesDiv.innerHTML = ''; // Clear the entries div
+      // Handle drag over the drop area and prevent the default action of opening in browser
+      function handleDragOver(event) {
+          event.preventDefault();
+          event.target.classList.add('drag-drop-area-active');
+      }
 
-  // Clear local storage
-  localStorage.removeItem('entries');
-}
+      // Handle drag leave the drop area and prevent the default action of opening in browser
+      function handleDragLeave(event) {
+          event.preventDefault();
+          event.target.classList.remove('drag-drop-area-active');
+      }
+
+      // Handle drop, save to image input, and prevent the default action
+      function handleDrop(event) {
+          event.preventDefault();
+          event.target.classList.remove('drag-drop-area-active');
+          const droppedFile = event.dataTransfer.files[0];
+          const imageInput = document.getElementById('imageInput');
+          imageInput.files = event.dataTransfer.files;
+          event.target.innerHTML = '<p>Great! Your image has been uploaded</p>';
+      }
+      //function for testing to avoid over-poulating local storage
+      // function deleteAllEntries() {
+      //   var entriesDiv = document.getElementById('entries');
+      //   entriesDiv.innerHTML = ''; // Clear the entries div
+
+      //   // Clear local storage
+      //   localStorage.removeItem('entries');
+      // }
 
 function loadEntriesFromLocalStorage() {
   // Get existing entries from local storage
